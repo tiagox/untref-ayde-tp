aa811fc71a26b87b5603636e900f24825cebc9c0
Santiago Rojo <tiagox@gmail.com>
2013-06-12 01:37:20 -0300

    Limpieza del HTML de "horas por proyecto".

--- application/views/reports/horas_por_proyecto.php
+++ application/views/reports/horas_por_proyecto.php
@@ -3,9 +3,21 @@
 <h3>Correspondiente al mes de <?= $month_name ?> (<?= $month_period ?>) </h3>
 <div class="span8">
   <table class="table table-bordered table-striped">
-    <tr><th>Proyecto</th><th>Costo</th></tr>
-    <?php foreach ($report_rows as $row) : ?>
-    <tr><td><?= $row->project ?></td><td>$ <?= number_format(intval($row->monthly_cost)) ?></td></tr>
-    <?php endforeach; ?>
+    <thead>
+      <tr>
+        <th>Proyecto</th>
+        <th>Costo</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($report_rows as $row) : ?>
+
+      <tr>
+        <td><?= $row->project ?></td>
+        <td>$ <?= number_format(intval($row->monthly_cost)) ?></td>
+      </tr>
+      <?php endforeach; ?>
+
+    </tbody>
   </table>
 </div>
