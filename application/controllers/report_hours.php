4275b32d3532b213d965777488cb670fc4da49fe
Santiago Rojo <tiagox@gmail.com>
2013-06-12 02:12:33 -0300

    Persistencia de las horas reportadas.

--- application/controllers/report_hours.php
+++ application/controllers/report_hours.php
@@ -15,10 +15,31 @@ class Report_Hours extends CI_Controller {
 
   public function index()
   {
+    if ($this->input->post()) {
+      $this->load->model('Reported_Hour');
+
+      $user_id = $this->input->post('user');
+      $week_id = $this->input->post('week');
+
+      $projects = $this->input->post('projects');
+
+      foreach ($projects as $project_id => $hours) {
+        if ($hours >= 0) {
+          $this->Reported_Hour->save($project_id, $user_id, $week_id, $hours);
+        }
+      }
+
+      redirect('home');
+    }
+
     $this->load->helper('form');
     $this->load->model('Project');
 
     $projects = $this->Project->get_all_active();
+
+    $this->load->model('User');
+
+    $users = $this->User->parse_to_select($this->User->get_all());
 
     $this->load->model('Week');
 
@@ -29,6 +50,7 @@ class Report_Hours extends CI_Controller {
     $this->load->view('layout/header', array('title' => 'Soluciones informaticas'));
     $this->load->view('layout/begin_content', array('selected' => 'report_hours'));
     $this->load->view('report_hours/index', array(
+      'users' => $users,
       'weeks' => $weeks,
       'last_week' => $last_week,
       'projects' => $projects
--- application/models/reported_hour.php
+++ application/models/reported_hour.php
@@ -7,6 +7,23 @@ class Reported_Hour extends CI_Model {
   public $week_id;
   public $hours;
 
+  public function save($project_id, $user_id, $week_id, $hours)
+  {
+    $this->db->delete('reported_hours', array(
+        'project_id' => $project_id,
+        'user_id' => $user_id,
+        'week_id' => $week_id
+      )
+    );
+
+    $this->project_id = $project_id;
+    $this->user_id = $user_id;
+    $this->week_id = $week_id;
+    $this->hours = $hours;
+
+    return $this->db->insert('reported_hours', $this);
+  }
+
   public function get_cost_report($month)
   {
     $this->db->select('project');
