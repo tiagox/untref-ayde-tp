092513169a8999ac7baae3058076c771775ab139
Santiago Rojo <tiagox@gmail.com>
2013-06-12 01:30:44 -0300

    Descripción del reporte.

--- application/views/reports/index.php
+++ application/views/reports/index.php
@@ -1,7 +1,10 @@
 <h1>Generar reporte</h1>
 <h2>Reporte de horas por proyecto</h2>
-<table class="table">
-  <tr>
-    <td><?php echo anchor('reports/horas_por_proyecto', 'Ver reporte', array('class' => 'btn')); ?></td>
-  </tr>
-</table>
+<div class="span8">
+  <table class="table table-bordered">
+    <tr>
+      <td class="span2"><?php echo anchor('reports/horas_por_proyecto', 'Ver reporte', array('class' => 'btn')); ?></td>
+      <td>Listar el costo de los recursos consumidos por cada proyecto, a mes cerrado al momento de la generación del reporte.</td>
+    </tr>
+  </table>
+</div>
