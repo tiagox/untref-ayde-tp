e72897739cd55458f1f6262f1be96dc1394b4481
Santiago Rojo <tiagox@gmail.com>
2013-06-15 21:27:19 -0300

    Se agrega CSS para la vista de impresión.

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/vendors/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="/vendors/bootstrap/css/bootstrap-responsive.css">
+    <link rel="stylesheet" type="text/css" href="/style/print.css" media="print">
   </head>
   <body>
