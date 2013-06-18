ba156130acef3c954e05a8c3916146b36500c0ea
mrodriguezinbox <zxv@omega.(none)>
2013-06-18 00:11:48 -0300

    Sumador dinámico de horas.

--- application/views/report_hours/index.php
+++ application/views/report_hours/index.php
@@ -1,3 +1,24 @@
+<script>
+// Not working :(
+window.onload = update_hours_sum();
+
+function update_hours_sum()
+{
+  var number_of_projects = $('[name^=project]').length;
+  var sum = 0;
+  for(var i = 1; i <= number_of_projects; i++)
+  {
+    sum = sum + Number($("[name='projects[" + i + "]']").val());
+  }
+  $('#hours_sum').html(sum);
+  if(sum > 40)
+  {
+    $('#hours_sum').css({color:'red'});
+  } else {
+    $('#hours_sum').css({color:'blue'});
+  }
+}
+</script>
 <h1>Reportar horas</h1>
 <?php echo form_open(); ?>
 
@@ -26,13 +47,14 @@
       echo form_input(array(
         'name' => 'projects[' . $project->id . ']',
         'type' => 'number',
-        'value' => $hours
+        'value' => $hours,
+	'onKeyUp' => 'update_hours_sum()'
       )); ?> horas</td>
     </tr>
     <?php endforeach; ?>
 
     <tr>
-      <td>&nbsp;</td>
+      <td><div style='float:left'>Horas cargadas:&nbsp</div><div id='hours_sum' style='font-size:25px'></div></td>
       <td><?php echo form_submit(array(
         'value' => 'Guardar',
         'class' => 'btn btn-primary'
