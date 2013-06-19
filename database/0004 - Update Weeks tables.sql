deae78541ea88e560301c5b34f8e886d7c1f5bc1
Santiago Rojo <tiagox@gmail.com>
2013-06-19 00:29:38 -0300

    Corregido error en sentencia SQL.

--- database/0004 - Update Weeks tables.sql
+++ database/0004 - Update Weeks tables.sql
@@ -4,4 +4,4 @@ SET time_zone = "+00:00";
 -- Update weeks table
 ALTER TABLE `weeks`
   ADD COLUMN `week_in_month` INT(11) NOT NULL  AFTER `end` ,
-  ADD COLUMN `year` INT(11) NOT NULL  AFTER `month`
+  ADD COLUMN `year` INT(11) NOT NULL  AFTER `month` ;
