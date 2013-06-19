63c07ff878dea72d506099bba63f2d3564cf6819
Santiago Rojo <tiagox@gmail.com>
2013-06-18 21:18:51 -0300

    Se agrega la posibilidad de incluir archivos .js desde los controladores.

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,4 +1,10 @@
   <script src="/vendors/jquery/jquery-1.10.1.js"></script>
   <script src="/vendors/bootstrap/js/bootstrap.js"></script>
+  <?php if (isset($jsFiles)) : ?>
+  <?php foreach ($jsFiles as $jsFile) : ?>
+
+  <script src="<?= $jsFile ?>"></script>
+  <?php endforeach; ?>
+  <?php endif; ?>
   </body>
 </html>
