d3afb997596b6d68349e46ceaaf4652b652c36b5
Santiago Rojo <tiagox@gmail.com>
2013-07-02 22:40:07 -0300

    Se habilita jQuery UI en todas las paginas del proyecto.

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,4 +1,5 @@
   <script src="/vendors/jquery/jquery-1.10.1.js"></script>
+  <script src="/vendors/jquery-ui/js/jquery-ui-1.10.3.custom.js"></script>
   <script src="/vendors/bootstrap/js/bootstrap.js"></script>
   <script src="/vendors/bootbox/bootbox.js"></script>
   <?php if (isset($jsFiles)) : ?>
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/vendors/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="/vendors/bootstrap/css/bootstrap-responsive.css">
+    <link rel="stylesheet" type="text/css" href="/vendors/jquery-ui/css/flick/jquery-ui-1.10.3.custom.css">
     <link rel="stylesheet" type="text/css" href="/style/print.css" media="print">
     <?php if (isset($cssFiles)) : ?>
     <?php foreach ($cssFiles as $cssFile) : ?>
